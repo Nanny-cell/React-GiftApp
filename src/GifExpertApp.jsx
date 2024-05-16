c4f4978492f1dcca14a865cc102d1bf08d390f1e
Nanni <danitza.p.96@gmail.com>
2024-05-16 13:07:17 -0400

    Implementar buscador de gifts

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react';
-import { AddCategory } from './components/AddCategory';
+import { AddCategory, GifGrid } from './components';
 
 export const GifExpertApp = () => {
 
-  const [ categories, setCategories ] = useState( ['One Punch', 'Dragon Ball'] );
+  const [ categories, setCategories ] = useState( ['One Punch'] );
 
   const onAddCategory = (newCategory) => {
-    //console.log(newCategory)
-    //categories.push(newCategory);
+
+    if ( categories.includes(newCategory) ) return;
     setCategories( [ newCategory, ...categories ] )
-    //setCategories ( cat => [...cat, 'Valorant'] )
+
   }
   
   return (
     <>
-        {/* titulo */}
+
         <h1>GifExpertApp</h1>
 
-        {/*Input */}
         <AddCategory 
-        //setCategories = { setCategories }
-        onNewCategory = { value => onAddCategory(value)}
-        
+        onNewCategory = { value => onAddCategory(value) }
         />
 
-        {/* Listado de Gif */}
-        <ol>
-          { categories.map( category =>{
-            return <li key ={ category } > {category} </li>
-          } ) }
-        </ol>
+      { 
+        categories.map( category => (
+          <GifGrid 
+            key={category} 
+            category={category} />
+        ))
+      }
+
     </>
   )
 }
